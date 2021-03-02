be3fe11d6c818e7242b4a4f05f9b0d9cb5e339bf
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-02 11:20:40 -0600

    Se agrego la variable de tipo JMenuBar 'menuBar' en la clase 'Ventana'

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,8 +1,17 @@
+import javax.swing.SwingUtilities;
 
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Ventana();
+				}
+		});
 
 	}
 
--- src/Ventana.java
+++ src/Ventana.java
@@ -1,6 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+
 
 public class Ventana extends JFrame{
 
+	JMenuBar menuBar;
+	JMenu menuConversores;
+	JMenuItem menuItemDistancias, menuItemTemperaturas, menuItemMoneda;
+	
 	public Ventana() {
 		
 		
