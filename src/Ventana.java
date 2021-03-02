0921e0861787a390ee4d14eac50e919768f769f6
RicardoReyes44 <rickyboy1002@gmail.com>
2021-03-02 16:34:00 -0600

    Se agregaron las variables 'gbc' de tipo GridBagLayout y 'gbc' de tipo GridBagConstraints

--- src/Ventana.java
+++ src/Ventana.java
@@ -2,6 +2,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,6 +18,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JToolBar;
 import javax.swing.KeyStroke;
 import javax.swing.WindowConstants;
 
@@ -25,25 +28,31 @@ public class Ventana extends JFrame{
 	JMenuBar menuBar;
 	JInternalFrame IF_Dentro;
 	JPanel panel;
+	GridBagConstraints gbc = new GridBagConstraints();
+	GridBagLayout gbl = new GridBagLayout();
+	JToolBar barraDeHerramientas;
 	JMenu menuMasters, menuBooking, menuTestPerform, menuPrinting, menuLaoReports,
           menuSettings, menuUtilities, menuWindow, menuHelp, menuTransaction;
-	JButton btnAdd = new JButton();
-	JButton btnEdit = new JButton();
-	JButton btnBack = new JButton();
-	JButton btnNext = new JButton();
-	JButton btnList = new JButton();
-	JButton btnSave = new JButton();
-	JButton btnPrint = new JButton();
-	JButton btnTest = new JButton();
-	JButton btnCancel = new JButton();
-	JButton btnSettings = new JButton();
-	JButton btnDelete = new JButton();
-	JButton btnSlip = new JButton();
-	JButton btnExit = new JButton();
+	JButton btnAdd;
+	JButton btnEdit;
+	JButton btnBack;
+	JButton btnNext;
+	JButton btnList;
+	JButton btnSave;
+	JButton btnPrint;
+	JButton btnTest;
+	JButton btnCancel;
+	JButton btnSettings;
+	JButton btnDelete;
+	JButton btnSlip;
+	JButton btnExit;
 
 	public Ventana() {
 
-		getContentPane().setLayout(new BorderLayout());
+		barraDeHerramientas = new JToolBar("Barra de herramientas");
+		barraDeHerramientas.add(new JButton("AS"));
+
+		getContentPane().setLayout(gbl);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setSize(1350, 720);
 		setLocationRelativeTo(null);
@@ -62,43 +71,7 @@ public class Ventana extends JFrame{
 	    menuWindow = new JMenu("Window");
 	    menuHelp = new JMenu("Help");
 
-	    menuBar.add(menuMasters);
-	    menuBar.add(menuBooking);
-	    menuBar.add(menuTestPerform);
-	    menuBar.add(menuPrinting);
-	    menuBar.add(menuTransaction);
-	    menuBar.add(menuLaoReports);
-	    menuBar.add(menuSettings);
-	    menuBar.add(menuUtilities);
-	    menuBar.add(menuWindow);
-	    menuBar.add(menuHelp);
-	    menuBar.add(new JButton("AS"));
-	    
-	    
-	    setJMenuBar(menuBar);
-	    
-	    // ---------- Barra de herramientas ---------------
-	 	JDesktopPane dp = new JDesktopPane();
-	 	
-	 	IF_Dentro = new JInternalFrame();
-		IF_Dentro.getContentPane().setLayout(new FlowLayout());
-		                                             // HIDE_ON_CLOSE para cerrar, pero se puede volver a abrir
-		IF_Dentro.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		IF_Dentro.setSize(900, 70);
-		IF_Dentro.setTitle("Barra de herramientas");
-		
-		// Boton para minimizar y convertir en icono
-		IF_Dentro.setIconifiable(true);
-		
-		// Boton para maximizar
-		IF_Dentro.setResizable(true);
-		
-		// Boton para cerrar
-		IF_Dentro.setClosable(true);
-		
-		IF_Dentro.setVisible(true);
-		
-        btnAdd = new JButton("Add");
+	    btnAdd = new JButton("Add");
         btnEdit = new JButton("Edit");
         btnBack = new JButton("Back");
         btnNext = new JButton("Next");
@@ -112,19 +85,55 @@ public class Ventana extends JFrame{
         btnSlip = new JButton("Slip");
         btnExit = new JButton("Exit");
         
-        IF_Dentro.add(btnAdd);
-        IF_Dentro.add(btnEdit);
-        IF_Dentro.add(btnBack);
-        IF_Dentro.add(btnNext);
-        IF_Dentro.add(btnList);
-        IF_Dentro.add(btnSave);
-        IF_Dentro.add(btnPrint);
-        IF_Dentro.add(btnTest);
-        IF_Dentro.add(btnCancel);
-        IF_Dentro.add(btnSettings);
-        IF_Dentro.add(btnDelete);
-        IF_Dentro.add(btnSlip);
-        IF_Dentro.add(btnExit);
+        barraDeHerramientas.add(btnAdd);
+        barraDeHerramientas.add(btnEdit);
+        barraDeHerramientas.add(btnBack);
+        barraDeHerramientas.add(btnNext);
+        barraDeHerramientas.add(btnList);
+        barraDeHerramientas.add(btnSave);
+        barraDeHerramientas.add(btnPrint);
+        barraDeHerramientas.add(btnTest);
+        barraDeHerramientas.add(btnCancel);
+        barraDeHerramientas.add(btnSettings);
+        barraDeHerramientas.add(btnDelete);
+        barraDeHerramientas.add(btnSlip);
+        barraDeHerramientas.add(btnExit);
+	    
+	    menuBar.add(menuMasters);
+	    menuBar.add(menuBooking);
+	    menuBar.add(menuTestPerform);
+	    menuBar.add(menuPrinting);
+	    menuBar.add(menuTransaction);
+	    menuBar.add(menuLaoReports);
+	    menuBar.add(menuSettings);
+	    menuBar.add(menuUtilities);
+	    menuBar.add(menuWindow);
+	    menuBar.add(menuHelp);
+	    menuBar.add(barraDeHerramientas);
+	    
+	    
+	    setJMenuBar(menuBar);
+	    
+
+	 	JDesktopPane dp = new JDesktopPane();
+
+	 	IF_Dentro = new JInternalFrame();
+		IF_Dentro.getContentPane().setLayout(new FlowLayout());		
+		                                             // HIDE_ON_CLOSE para cerrar, pero se puede volver a abrir
+		IF_Dentro.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		IF_Dentro.setSize(900, 70);
+		IF_Dentro.setTitle("");
+		
+		// Boton para minimizar y convertir en icono
+		IF_Dentro.setIconifiable(true);
+		
+		// Boton para maximizar
+		IF_Dentro.setResizable(true);
+		
+		// Boton para cerrar
+		IF_Dentro.setClosable(true);
+		
+		IF_Dentro.setVisible(true);
         
     	dp.add(IF_Dentro);
 		add(dp, BorderLayout.CENTER);
